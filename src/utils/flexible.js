f25ac0ee838c429935d4665b7acb0b6db6699eb9
lidoudoou <lidoudoou@gmail.com>
2022-07-19 10:51:14 +0800

    import vueuse

--- src/utils/flexible.js
+++ src/utils/flexible.js
@@ -1,9 +1,11 @@
 import { computed } from 'vue'
 import { PC_DEVICE_WIDTH } from '../constants'
+import { useWindowSize } from '@vueuse/core'
 
+const { width } = useWindowSize()
 /**
  * 判断当前是否是移动端
  */
 export const isMobileTermial = computed(() => {
-  return document.documentElement.clientWidth < PC_DEVICE_WIDTH
+  return width.value < PC_DEVICE_WIDTH
 })
