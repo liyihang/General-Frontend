2868cb99ed0d3fb34ecd6cc0e99d966af8746689
lidoudoou <lidoudoou@gmail.com>
2022-07-24 00:08:13 +0800

    use useVmodel instead  modelValue

--- src/libs/popup/index.vue
+++ src/libs/popup/index.vue
@@ -3,13 +3,13 @@
     <teleport to="body">
       <!-- 蒙版 -->
       <transition name="fade">
-        <div v-if="modelValue" class=" w-screen h-screen bg-zinc-900/80 z-40 fixed top-0 left-0"
-             @click="emits('update:modelValue', false)">
+        <div v-if="isVisible" class=" w-screen h-screen bg-zinc-900/80 z-40 fixed top-0 left-0"
+             @click="isVisible = false">
         </div>
       </transition>
       <!-- 内容 -->
       <transition name="pop-down-up">
-        <div v-if="modelValue" v-bind="$attrs" class=" w-screen bg-white z-50 fixed bottom-0">
+        <div v-if="isVisible" v-bind="$attrs" class=" w-screen bg-white z-50 fixed bottom-0">
           <slot></slot>
         </div>
       </transition>
@@ -18,16 +18,28 @@
 </template>
 
 <script setup>
-import { useScrollLock } from '@vueuse/core';
+import { useScrollLock, useVModel } from '@vueuse/core';
+import { watch } from 'vue';
 const props = defineProps({
   modelValue: {
     required: true,
     Type: Boolean
   }
 })
-const emits = defineEmits(['update:modelValue'])
+defineEmits(['update:modelValue'])
+// useVmodel
+const isVisible = useVModel(props)
 // 锁定状态
 const isLocked = useScrollLock(document.body)
+// 同步状态
+watch(
+  isVisible,
+  (val) => {
+    isLocked.value = val
+  },
+  {
+    immediate: true
+  })
 </script>
 
 <style lang="scss" scoped>
