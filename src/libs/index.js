e4d0040e3fe53c273533e1175d5bd26613953256
lidoudoou <lidoudoou@gmail.com>
2022-07-24 00:22:00 +0800

    feat:use defineAsyncComponent & glob optimization app install

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,10 +1,18 @@
-import svgIcon from './svg-icon/index.vue'
-import popup from './popup/index.vue'
+// import svgIcon from './svg-icon/index.vue'
+// import popup from './popup/index.vue'
+import { defineAsyncComponent } from 'vue'
 
 
 export default {
   install(app) {
-    app.component('m-svg-icon', svgIcon),
-      app.component('m-popup', popup)
+    // 自动注册
+    // 1.获取当前路径下的所有index.vue
+    const components = import.meta.glob('./*/index.vue')
+    // 2.遍历组件
+    for (const [fullPath, fn] of Object.entries(components)) {
+      const componentName = 'm-' + fullPath.replace('./', '').split('/')[0]
+      // 3.利用app.components
+      app.component(componentName, defineAsyncComponent(fn))
+    }
   }
 } 
