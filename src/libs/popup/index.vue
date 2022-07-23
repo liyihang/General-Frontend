c72e8bd44ad8f0dff67d6ad9c17006f1bbf04085
lidoudoou <lidoudoou@gmail.com>
2022-07-23 23:44:21 +0800

    teleport to body

--- src/libs/popup/index.vue
+++ src/libs/popup/index.vue
@@ -2,9 +2,15 @@
   <div>
     <teleport to="body">
       <!-- 蒙版 -->
-      <div>蒙版</div>
+      <transition name="fade">
+        <div class=" w-screen h-screen bg-zinc-900/80 z-40 fixed top-0 left-0"></div>
+      </transition>
       <!-- 内容 -->
-      <div>内容</div>
+      <transition name="pop-down-up">
+        <div v-bind="$attrs" class=" w-screen bg-white z-50 fixed bottom-0">
+          <slot></slot>
+        </div>
+      </transition>
     </teleport>
   </div>
 </template>
@@ -14,4 +20,23 @@
 </script>
 
 <style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.pop-down-up-enter-active,
+.pop-down-up-leave-active {
+  transition: all 0.3s;
+}
+
+.pop-down-up-enter-from,
+.pop-down-up-enter-to {
+  transform: translateY(100%);
+}
 </style>
