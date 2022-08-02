bc7665b546115e5bf01d0184ccc81dbb80ff9bfe
lidoudoou <lidoudoou@gmail.com>
2022-08-02 18:00:19 +0800

    feat:弹窗

---
+++ src/libs/popover/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class=" relative" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
+    <div>
+      <!--named slot  show popover -->
+      <slot name="reference"></slot>
+    </div>
+  </div>
+  <!-- 气泡展示 -->
+  <transition name="slide">
+    <div v-show="isVisible" class=" absolute p-1 z-20 bg-white border rounded-md">
+      <!-- slot show container -->
+      <slot />
+    </div>
+  </transition>
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const isVisible = ref(false)
+// 鼠标移入触发
+const onMouseEnter = () => {
+
+}
+// 鼠标移出触发
+const onMouseLeave = () => {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 0.3s, transform 0.3s;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateY(20px);
+}
+</style>
--- src/views/layout/components/header/header-theme.vue
+++ src/views/layout/components/header/header-theme.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    theme
-  </div>
+  <m-popover>
+    <!--named slot  show popover -->
+    <template #reference>
+      <m-svg-icon name="theme-light"
+                  class="w-4 h-4 p-1 cursor-pointer rounded-sm duration-200 outline-none hover:bg-zinc-100/60"
+                  fillClass="fill-zinc-900"></m-svg-icon>
+    </template>
+    <!-- slot show container -->
+  </m-popover>
 </template>
 
 <script setup>
