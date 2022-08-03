1eb7e66f74e7b244d65c3036369de15dcaad91a4
lidoudoou <lidoudoou@gmail.com>
2022-08-03 11:50:01 +0800

    feat:theme define

--- src/libs/popover/index.vue
+++ src/libs/popover/index.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class=" relative" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
-    <div>
-      <!--named slot  show popover -->
-      <slot name="reference"></slot>
+  <div>
+    <div class=" relative" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
+      <div>
+        <!--named slot  show popover -->
+        <slot name="reference"></slot>
+      </div>
     </div>
+    <!-- 气泡展示 -->
+    <transition name="slide">
+      <div v-show="isVisible" class=" absolute p-1 z-20 bg-white border rounded-md">
+        <!-- slot show container -->
+        <slot />
+      </div>
+    </transition>
   </div>
-  <!-- 气泡展示 -->
-  <transition name="slide">
-    <div v-show="isVisible" class=" absolute p-1 z-20 bg-white border rounded-md">
-      <!-- slot show container -->
-      <slot />
-    </div>
-  </transition>
-
 </template>
 
 <script setup>
@@ -21,10 +22,11 @@ import { ref } from 'vue';
 const isVisible = ref(false)
 // 鼠标移入触发
 const onMouseEnter = () => {
-
+  isVisible.value = true
 }
 // 鼠标移出触发
 const onMouseLeave = () => {
+  isVisible.value = false
 
 }
 </script>
--- src/views/layout/components/header/header-theme.vue
+++ src/views/layout/components/header/header-theme.vue
@@ -7,11 +7,39 @@
                   fillClass="fill-zinc-900"></m-svg-icon>
     </template>
     <!-- slot show container -->
+    <div class=" w-[140px] overflow-hidden">
+      <div class=" flex items-center p-1 cursor-pointer rounded hover:bg-zinc-100/60" v-for="item in themeArr"
+           :key="item.id">
+        <m-svg-icon :name="item.icon" class=" w-1.5 h-1.5 mr-1" fillClass="fill-zinc-900"></m-svg-icon>
+        <span class=" text-zinc-800 text-sm">{{ item.name }}</span>
+      </div>
+    </div>
   </m-popover>
 </template>
 
 <script setup>
+import { THEME_LIGHT, THEME_DARK, THEME_SYSTEM } from '@/constants'
 
+const themeArr = [
+  {
+    id: '0',
+    type: THEME_LIGHT,
+    icon: 'theme-light',
+    name: '极简白'
+  },
+  {
+    id: '1',
+    type: THEME_DARK,
+    icon: 'theme-dark',
+    name: '极简黑'
+  },
+  {
+    id: '2',
+    type: THEME_SYSTEM,
+    icon: 'theme-system',
+    name: '跟随系统'
+  }
+]
 </script>
 
 <style lang="scss" scoped>
