8afd9e16297ba0072116dbd05c7a8d205334d32d
lidoudoou <lidoudoou@gmail.com>
2022-08-04 18:00:49 +0800

    pc page

--- src/views/main/components/navigation/pc/index.vue
+++ src/views/main/components/navigation/pc/index.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>
-
+  <div class="bg-white sticky top-0 left-0 w-full z-10">
+    <ul
+      class="w-[800px] relative flex flex-wrap justify-center overflow-x-auto px-[10px] py-1 text-xs text-zinc-600 duration-200 overflow-auto mx-auto"
+    >
+      <!-- 箭头 -->
+      <div
+        class="absolute right-1 bottom-1 z-20 p-1 rounded cursor-pointer duration-200 hover:bg-zinc-200"
+      >
+        <m-svg-icon name="unfold" fillClass="fill-zinc-900"></m-svg-icon>
+      </div>
+      <!-- item -->
+      <li></li>
+    </ul>
   </div>
 </template>
 <script>
-export default {
-
-}
+export default {}
 </script>
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
