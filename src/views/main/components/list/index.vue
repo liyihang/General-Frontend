23ae598481cf802a8054ed4d9dd7310728c59763
lidoudoou <lidoudoou@gmail.com>
2022-08-08 11:40:57 +0800

    main item

--- src/views/main/components/list/index.vue
+++ src/views/main/components/list/index.vue
@@ -1,7 +1,26 @@
 <template>
-  <div></div>
+  <div>
+    <item-vue v-for="item in pexelsData" :key="item.id" :data="item"></item-vue>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { getPexelsList } from '@/api/pexels.js'
+import { ref } from 'vue'
+import itemVue from './item.vue'
 
+/**
+ *构建请求参数
+ */
+let query = {
+  page: 1,
+  size: 20,
+}
+const pexelsData = ref([])
+const getPexelsData = async () => {
+  const res = await getPexelsList(query)
+  pexelsData.value = res.list
+}
+getPexelsData()
+</script>
 <style lang="scss" scoped></style>
--- src/views/main/components/list/item.vue
+++ src/views/main/components/list/item.vue
@@ -1,7 +1,61 @@
 <template>
-  <div></div>
+  <div
+    class="bg-white dark:bg-zinc-900 xl:dark:bg-zinc-800 rounded pb-1 h-[280px] w-[230px]"
+  >
+    <div class="relative w-full rounded cursor-zoom-in group">
+      <!-- 图片  -->
+      <img class="w-full rounded bg-transparent" :src="data.photo" />
+      <!-- 遮罩层 -->
+      <div
+        class="hidden opacity-0 w-full h-full bg-zinc-900/50 absolute top-0 left-0 rounded duration-300 group-hover:opacity-100 xl:block"
+      >
+        <!-- 分享 -->
+        <m-button class="absolute top-1.5 left-1.5">分享</m-button>
+        <!-- 点赞 -->
+        <m-button
+          class="absolute top-1.5 right-1.5"
+          type="info"
+          icon="heart"
+          iconClass="fill-zinc-900 dark:fill-zinc-200"
+        ></m-button>
+
+        <!-- 下载 -->
+        <m-button
+          class="absolute bootom-1.5 left-1.5 bg-zinc-100/70"
+          type="info"
+          size="small"
+          icon="download"
+          iconClass="fill-zinc-900 dark:fill-zinc-200"
+        ></m-button>
+        <!-- 放大 -->
+        <m-button
+          class="absolute bootom-1.5 right-1.5 bg-zinc-100/70"
+          type="info"
+          size="small"
+          icon="full"
+          iconClass="fill-zinc-900 dark:fill-zinc-200"
+        ></m-button>
+      </div>
+    </div>
+    <!-- 标题 -->
+    <p class="text-sm mt-1 font-bold text-zinc-900 dark:text-zinc-200 px-1">
+      {{ data.title }}
+    </p>
+    <!-- author -->
+    <div class="flex items-center mt-1 px-1">
+      <img class="w-2 h-2 rounded-full" :src="data.avatar" alt="" />
+      <span class="text-sm text-zinc-500 ml-1">{{ data.author }}</span>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+defineProps({
+  data: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
 
 <style lang="scss" scoped></style>
